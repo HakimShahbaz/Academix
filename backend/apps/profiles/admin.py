f4853aeac3db15765785f0023029fc15a8814c23
Amirreza <amirreza13832004@gmail.com>
2026-07-05 20:12:20 +0330

    feat(profiles): configure StudentProfile admin

--- backend/apps/profiles/admin.py
+++ backend/apps/profiles/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import StudentProfile
 
-admin.site.register(StudentProfile)
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "student_number",
+        "user",
+        "initial_enrollment_date")
+    search_fields = (
+        "student_number",
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+    )
+    ordering = ("student_number",)
