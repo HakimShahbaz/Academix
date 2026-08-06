ace1d13c50e7cbd08ed8ea90113c05de3a42235b
Amirreza <amirreza13832004@gmail.com>
2026-08-06 20:50:58 +0330

    feat(core): display live statistics on dashboard

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from apps.attendances.models import Attendance
+from apps.courses.models import Course, Section
+from apps.exams.models import Exam, Grade
+from apps.enrollments.models import Enrollment
+from apps.profiles.models import StudentProfile, TeacherProfile, EmployeeProfile
+
+
 @login_required()
 def dashboard(request):
-    return render(request, "core/dashboard.html")
+    context = {
+        "student_count": StudentProfile.objects.count(),
+        "teacher_count": TeacherProfile.objects.count(),
+        "employee_count": EmployeeProfile.objects.count(),
+        "course_count": Course.objects.count(),
+        "section_count": Section.objects.count(),
+        "enrollment_count": Enrollment.objects.count(),
+        "attendance_count": Attendance.objects.count(),
+        "exam_count": Exam.objects.count(),
+        "grades_count": Grade.objects.count(),
+    }
+
+    return render(request, "core/dashboard.html", context)
